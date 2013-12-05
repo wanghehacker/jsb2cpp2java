199d2bad46af64c7c05894a3804c8e49e876d49a
wanghe-laptop <wanghewin8@163.com>
2013-12-05 10:32:07 +0800

    删除obj目录的文件

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /cygdrive/e/cocos2dx/cocos2d-x-2.2/cocos2d-x-2.2/projects/Jsb2cpp2Java/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /cygdrive/e/cocos2dx/cocos2d-x-2.2/cocos2d-x-2.2/projects/Jsb2cpp2Java/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/cygdrive/e/cocos2dx/cocos2d-x-2.2/cocos2d-x-2.2/projects/Jsb2cpp2Java/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
